58f1759c118f4aa8b13a2ccd8f1978b3a2f48be6
Oranges-sour <873516725@qq.com>
2021-12-08 23:49:38 +0800

    添加了敌人血条

---
+++ Classes/EnemyHitPointBar.cpp
@@ -0,0 +1,12 @@
+#include "EnemyHitPointBar.h"
+
+EnemyHitPointBar* EnemyHitPointBar::create(basic_Enemy* enemy)
+{
+	auto b = new (std::nothrow)EnemyHitPointBar();
+	if (b && b->init(enemy)) {
+		b->autorelease();
+		return b;
+	}
+	CC_SAFE_DELETE(b);
+	return nullptr;
+}
---
+++ Classes/EnemyHitPointBar.h
@@ -0,0 +1,21 @@
+#ifndef __ENEMY_HITPOINT_BAR_H__
+#define __ENEMY_HITPOINT_BAR_H__
+
+#include "cocos2d.h"
+USING_NS_CC;
+
+#include "Enemy.h"
+
+class EnemyHitPointBar : public Node {
+public:
+    static EnemyHitPointBar* create(basic_Enemy* enemy);
+    bool init(basic_Enemy* enemy);
+    EnemyHitPointBar();
+    virtual ~EnemyHitPointBar() {}
+
+private:
+    DrawNode* drawNode = nullptr;
+    basic_Enemy* enemy = nullptr;
+};
+
+#endif
